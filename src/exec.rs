cee299bdaabf6aa0fa18f1364ffd78666b3aeca3
Eric Kidd <git@randomhacks.net>
2016-03-23 07:18:35 -0400

    Call `exec` with arguments passed to `credentials-to-env`

---
+++ src/exec.rs
@@ -0,0 +1,80 @@
+//! A wrapper around the C library's `execv` function.
+
+use errno::{Errno, errno};
+use libc;
+use std::error::{self, Error};
+use std::ffi::{CString, NulError};
+use std::fmt;
+use std::ptr;
+
+/// Represents an error calling `exec`.
+#[derive(Debug)]
+pub enum ExecError {
+    /// One of the strings passed to `execv` contained an internal null byte
+    /// and can't be passed correctly to C.
+    BadArgument(NulError),
+    /// An error was returned by the system.
+    Errno(Errno),
+}
+
+impl error::Error for ExecError {
+    fn description(&self) -> &str {
+        match self {
+            &ExecError::BadArgument(_) => "bad argument to exec",
+            &ExecError::Errno(_) => "couldn't exec process",
+        }
+    }
+    fn cause(&self) -> Option<&error::Error> {
+        match self {
+            &ExecError::BadArgument(ref err) => Some(err),
+            &ExecError::Errno(_) => None,
+        }
+    }
+}
+
+impl fmt::Display for ExecError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            &ExecError::BadArgument(ref err) =>
+                write!(f, "{}: {}", self.description(), err),
+            &ExecError::Errno(err) =>
+                write!(f, "{}: {}", self.description(), err),
+        }
+    }
+}
+
+impl From<NulError> for ExecError {
+    /// Convert a `NulError` into an `ExecError`.
+    fn from(err: NulError) -> ExecError {
+        ExecError::BadArgument(err)
+    }
+}
+
+/// Run `program` with `args`, completely replacing the currently running
+/// program.
+pub fn execvp(program: &str, args: &[&str]) -> Result<(), ExecError> {
+    // Add null terminations to our strings and our argument array,
+    // converting them into a C-compatible format.
+    let program_cstring = try!(CString::new(program.clone()));
+    let arg_cstrings = try!(args.iter().map(|arg| {
+        CString::new(arg.clone())
+    }).collect::<Result<Vec<_>, _>>());
+    let mut arg_charptrs: Vec<_> = arg_cstrings.iter().map(|arg| {
+        arg.as_bytes_with_nul().as_ptr() as *const i8
+    }).collect();
+    arg_charptrs.push(ptr::null());
+
+    // Use an `unsafe` block so that we can call directly into C.
+    let res = unsafe {
+        libc::execvp(program_cstring.as_bytes_with_nul().as_ptr() as *const i8,
+                     arg_charptrs.as_ptr())
+    };
+
+    // Handle our error result.
+    if res < 0 {
+        Err(ExecError::Errno(errno()))
+    } else {
+        // Should never happen.
+        Ok(())
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 extern crate credentials;
+extern crate errno;
+extern crate libc;
 
 use credentials::Secretfile;
 use std::env;
@@ -6,6 +8,8 @@ use std::error;
 use std::fs;
 use std::io::{self, Write};
 use std::process;
+
+mod exec;
 
 /// A nice, generic error type which can hold any error returned by any
 /// library we use, and to which the `try!` macro will automatically
@@ -28,6 +32,14 @@ fn helper() -> Result<(), Error> {
         let data = try!(credentials::file(path));
         let mut f = try!(fs::File::create(path));
         try!(f.write_all(data.as_bytes()));
+    }
+
+    // If we were supplied with command-line arguments, treat them as a
+    // command to exec.  This will replace the currently running binary.
+    let args: Vec<_> = env::args().collect();
+    let arg_refs: Vec<&str> = args.iter().map(|s| &s[..]).collect();
+    if args.len() >= 2 {
+        try!(exec::execvp(arg_refs[1], &arg_refs[1..]));
     }
 
     Ok(())
